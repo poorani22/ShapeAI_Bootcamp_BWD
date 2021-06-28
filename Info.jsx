dec3040179745b472b67ea47fa73524b804da2d3
poorani22 <83298801+poorani22@users.noreply.github.com>
2021-06-28 03:00:04 +0100

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Info() {
+  return(
+
+    <div class="note">
+      <h1> Javascript and React.js</h1>
+      <p> A basic web dev  bootcamp  with Javascript and react.js </p>
+    </div>
+    
+  );
+}
+
+export default Info;
